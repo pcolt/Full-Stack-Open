5e95af3d65961fe5f47f92106d3185f9d4a4e142
Pablo Colturi <pcolturi@fastmail.com>
2021-02-03 12:14:01 +0100

    commit sending data to/from json server

--- src/index.js
+++ src/index.js
@@ -1,17 +1,196 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import external module for making notes:
+import NoteImported from './components/Note'
+import axios from 'axios'
+
+
+//initialize an array of objects representing the notes to be displayed:
+const oldNotes = [
+	{
+		id: 1,
+		content: 'HTML is esay (old)',
+		date: '2019-05-30T17:30:31.098Z',
+		important: true
+	},
+	{
+		id: 2,
+		content: 'Browser can execute only JavaScript (old)',
+		date: '2019-05-30T18:39:34.091Z',
+		important: false
+	},
+	{
+		id: 3,
+		content: 'GET and POST are the most important methods of HTTP protocol (old)',
+		date: '2019-05-30T19:20:14.298Z',
+		important: true
+	}
+]
+
+//we could refactor a single note in its own component Note:
+const Note = ({ note, toggleImportance }) => {
+	return (
+		<li>{note.content}</li>
+	)
+}
+
+const App = (props) => {
+	//syntactic sugar for: 
+	//const notes = props.notes
+	//cool way:
+	//const {notes} = props
+	//an alternative would be just 'destructuring':
+	//const App = ({notes }) => { ...
+	//but in order to update page when new notes are added let's add them to the App component's state:
+	const [oldNotes, setOldNotes] = useState(props.oldNotes)
+	//and let's now use also 'db.json' file for our notes:
+	const [notes, setNotes] = useState([])
+	//a state storing user-submitted input:
+	const [newNote, setNewNote] = useState('a new note...')
+	//state of notes to be displayed:
+	const [showAll, setShowAll] = useState(true)
+
+	//HTML form for adding new notes
+	const addNote = (event) => {
+		event.preventDefault()
+		console.log('button clicked', event.target)
+		//create new object for new note
+		const noteObject = {
+			content: newNote,
+			date: new Date().toISOString(),
+			important: Math.random() < 0.5,
+			id: notes.length + 1,
+		}
+		//add note to array 'notes'
+		setNotes(notes.concat(noteObject))
+		setNewNote('a new note...')
+
+		//create new object (duplicate) with no id because better sever handle this itself
+		const noteObjectDb = {
+			content: newNote,
+			date: new Date().toISOString(),
+			important: Math.random() < 0.5,
+		}
+		//new object sent to server with axios and post method	
+		axios
+			.post('http://localhost:3001/notes', noteObjectDb)
+			.then(response => {
+				console.log(response)
+			})
+	}
+	//event handler to synchronize the changes in the input with the component's state:
+	const handleNoteChange = (event) => {
+		console.log(event.target.value)
+		setNewNote(event.target.value)
+	}
+
+	//store a list of all notes to be displayed depending on state 'showAll' is true or false, if false show only notes with important property set to 'true'
+	const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
+
+	const toggleImportanceOf = (id) => {
+		console.log('importance of ' + id + ' needs to be toggled')
+		//or using template string syntax added with ES6
+		console.log(`importance of ${id} needs to be toogled`)
+		//HTTP request to note's unique URL with PUT to replace entire note with imoportant field toggled
+		const url = `http://localhost:3001/notes/${id}`
+		const note = notes.find(n => n.id === id)
+		const changedNote = { ...note, important: !note.important }
+		axios
+			.put(url, changedNote)
+			.then(response => {
+				setNotes(notes.map(note => note.id !== id ? note : response.data))
+			})
+	}
+
+
+	//use axios to talk with json-server
+	//longer method:
+	/*const promise = axios.get('http://localhost:3001/notes')
+	console.log(promise)
+	promise.then(response => {
+		console.log(response)
+	})*/
+	//shorter syntax + use of side effects with useEffect() so that it is executed immediately after rendering:
+	const hook = () => {
+		console.log('effect')
+		axios
+			.get('http://localhost:3001/notes')
+			.then(response => {
+				console.log('promise fulfilled')
+				setNotes(response.data)
+			})
+	}
+	console.log('render', notes.length, 'notes: ', notes)
+	//second parameter [] specify that effect runs only with first render of component
+	useEffect(hook, [])
+
+	return (
+		<div>
+			<h1>Notes</h1>
+
+			{/*list of notes, first version*/}
+			<p>Very first version:</p>
+			<ul>
+				<li>{oldNotes[0].content}</li>
+				<li>{oldNotes[1].content}</li>
+				<li>{oldNotes[2].content}</li>
+			</ul>
+
+			<p>Second version using map (here 'show all/important' does not work):</p>
+			<ul>
+				{/*same result as above but with map*/}
+				{/*as any JS code in a JSX template it must be wrapped with {}*/}
+				{notes.map(note => 
+					//list items must have unique key
+					<li key={note.id}>{note.content}</li>
+				)}
+			</ul>
+
+
+			{/*button to show important/all notes*/}
+			<div>
+				<button onClick={() => setShowAll(!showAll)}>
+					show {showAll ? 'important' : 'all' }
+				</button>
+			</div>
+
+			<p>Third version refactoring Note in a separate component:</p>
+			<ul>
+				{notesToShow.map(note =>
+					<Note key={note.id} note={note} />	
+				)}
+			</ul>
+
+			<p>Fourth version uses Note as an external module:</p>
+			<ul>
+				{notesToShow.map(note =>
+					<NoteImported 
+						key={note.id} 
+						note={note} 
+						toggleImportance={() => toggleImportanceOf(note.id)}
+					/>
+				)}
+			</ul>
+
+			{/*form to add new notes*/}
+			<h1>Form</h1>
+			<form onSubmit={addNote}>
+				<input value={newNote} onChange={handleNoteChange}/>
+				<button type="submit">save</button>
+			</form>
+		</div>
+	)
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App oldNotes={oldNotes} />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//print the ids of each single note object stored in the array 'oldNotes'
+const result = oldNotes.map(note => note.id)
+console.log("result of map: ", result)
